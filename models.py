eae9eb8471e877782f46860aa4c485a4a380463a
Steven Bischoff <61480100+stevenbischoff@users.noreply.github.com>
2024-06-06 10:54:23 -0500

    Code cleanup

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 import torch
 import torch.nn as nn
@@ -11,34 +10,29 @@ class LSTM(nn.Module):
         super().__init__()
 
         self.padding_value = vocab['<pad>']
-        self.vocab_size = len(vocab)
 
-        self.hidden_size = hidden_size
-        self.batch_size = batch_size
-
-        self.embedding = nn.Embedding(self.vocab_size, embedding_size)
+        self.embedding = nn.Embedding(len(vocab), embedding_size)
         self.lstm = nn.LSTM(embedding_size, hidden_size, batch_first=True)
         self.dropout = nn.Dropout(0.2)
         self.h_2_o = nn.Linear(hidden_size, output_size)
+        
+        # activations: log_softmax for training, softmax for getting probabilities
         self.log_softmax = nn.LogSoftmax(dim=1)
-
         self.softmax = nn.Softmax(dim=-1)
 
-        self.train_losses = []
-        self.train_accuracies = []
-        self.test_losses = []
-        self.test_accuracies = []
-
-    def forward(self, input_tensor, lengths):
-        embeds = self.embedding(input_tensor)
+    def forward(self, idx, lengths):
+        embeds = self.embedding(idx)
+        # pack padded embeddings using lengths list for lstm layer
         packed_embeds = pack_padded_sequence(embeds, lengths, batch_first=True, enforce_sorted=False)
         packed_hidden, _ = self.lstm(packed_embeds)
+        # pad packed hidden layers using vocab padding value
         hidden, _ = pad_packed_sequence(packed_hidden, batch_first=True, padding_value=self.padding_value)
         hidden = self.dropout(hidden)
         # keep only the last unpadded hidden state using lengths
         hidden = torch.stack([h[length-1] for h, length in zip(hidden, lengths)])
         output = self.h_2_o(hidden)
         output = self.log_softmax(output)
+        
         return output
     
     def get_batch_hiddens_and_outputs(self, input_tensor, lengths, mode='numpy'):
@@ -58,6 +52,7 @@ class LSTM(nn.Module):
         packed_hidden, _ = self.lstm(packed_embeds)
         hidden, _ = pad_packed_sequence(packed_hidden, batch_first=True, padding_value=self.padding_value)
         output = self.h_2_o(hidden)
+        # apply softmax to get probabilities
         output = self.softmax(output)
 
         if mode == 'numpy':
@@ -88,14 +83,16 @@ class LSTM(nn.Module):
                 batch_hiddens, batch_outputs = self.get_batch_hiddens_and_outputs(input_tensor, lengths, mode=mode)
                 all_hiddens += batch_hiddens
                 all_outputs += batch_outputs
+                
         return all_hiddens, all_outputs
 
-    def evaluate_batch(self, X, lengths, y, loss_criterion): # assumes self.eval()
+    @torch.no_grad()
+    def evaluate_batch(self, idx, lengths, y, loss_criterion): # assumes self.eval()
         """
         Calculate loss and accuracy (# correct and # total) for a batch of inputs.
         Assumes self is in evaluation mode (self.eval()).
         Params:
-            X: tensor of shape (batch_size, max_length)
+            idx: tensor of shape (batch_size, max_length)
             lengths: list of integers
             y: tensor of shape (batch_size)
         Returns:
@@ -103,21 +100,22 @@ class LSTM(nn.Module):
             n_correct: int
             n_total: int
         """
-        outputs = self(X, lengths)
+        outputs = self(idx, lengths)
         predictions = outputs.topk(1)[1]
         # calculate loss
         loss = loss_criterion(outputs, y).item()    
         # calculate no. of correct predictions              
         n_correct = torch.sum(predictions == y.view(-1, 1)).item() 
 
-        return loss, (n_correct, len(X))
+        return loss, (n_correct, len(idx))
     
+    @torch.no_grad()
     def evaluate(self, dataloader, loss_criterion):
         """
         Calculate loss and accuracy for all inputs in a dataloader.
         Params:
             dataloader: DataLoader object for IMDBDataset
-            loss_criterion: loss function
+            loss_criterion: torch loss function
         Returns:
             loss: float
             acc: float
@@ -128,34 +126,35 @@ class LSTM(nn.Module):
         # set evaluation mode
         self.eval()
         # iterate over batches
-        with torch.no_grad():
-            for i, (input_tensor, lengths, category_tensor) in enumerate(dataloader): # iterate over batches
-                batch_loss, (batch_correct, batch_size) = self.evaluate_batch(input_tensor, lengths, category_tensor, loss_criterion)
-                losses.append(batch_loss)
-                n_correct += batch_correct
-                n_total += batch_size
+        for i, (idx, lengths, y) in enumerate(dataloader): # iterate over batches
+            batch_loss, (batch_correct, batch_size) = self.evaluate_batch(idx, lengths, y, loss_criterion)
+            losses.append(batch_loss)
+            n_correct += batch_correct
+            n_total += batch_size
         acc = n_correct/n_total
+        
         return np.mean(losses), acc
 
-    def train_batch(self, input_tensor, lengths, category_tensor, loss_criterion, optimizer):
+    def train_batch(self, idx, lengths, y, loss_criterion, optimizer):
         """
         Train the model on a single batch.
         Params:
-            input_tensor: tensor of shape (batch_size, max_length)
+            idx: tensor of shape (batch_size, max_length)
             lengths: list of integers
-            category_tensor: tensor of shape (batch_size)
-            loss_criterion: loss function
-            optimizer: optimizer
+            y: tensor of shape (batch_size)
+            loss_criterion: torch loss function
+            optimizer: torch optimizer
         """
         # reset gradients
         self.zero_grad()
         # get batch model output
-        output = self(input_tensor, lengths)
+        output = self(idx, lengths)
         # calculate loss
-        loss = loss_criterion(output, category_tensor)
+        loss = loss_criterion(output, y)
         # backpropagate
         loss.backward()
         optimizer.step()
+        
         return loss.item()
 
     def train_epoch(self, dataloader, loss_criterion, optimizer):
@@ -163,36 +162,33 @@ class LSTM(nn.Module):
         Train the model for one epoch.
         Params:
             dataloader: DataLoader object for IMDBDataset
-            loss_criterion: loss function
-            optimizer: optimizer
+            loss_criterion: torch loss function
+            optimizer: torch optimizer
         """
         # set training mode
         self.train() 
         # iterate over batches
-        for i, (input_tensor, lengths, category_tensor) in enumerate(dataloader):
-            batch_loss = self.train_batch(input_tensor, lengths, category_tensor, loss_criterion, optimizer)
+        for i, (idx, lengths, y) in enumerate(dataloader):
+            self.train_batch(idx, lengths, y, loss_criterion, optimizer)
 
     def fit(self, 
             train_dataloader,
             loss_criterion,
             optimizer, 
-            epochs=1,
+            epochs=1, # int >= 0
             track_train_stats=False, # bool
             track_test_stats=False, # bool
             test_dataloader=None, # DataLoader object for IMDBDataset
-            test_every=1, # int >= 1
             verbose=False # bool
             ):
         """
         Train the model for a specified number of epochs.
         Params:
             train_dataloader: DataLoader object for IMDBDataset
-            loss_criterion: loss function
-            optimizer: optimizer
-            epochs: int
+            loss_criterion: torch loss function
+            optimizer: torch optimizer
         """
-        # sanity checks
-        assert test_every >= 1, 'test_every must be >= 1'
+        # sanity check
         assert not track_test_stats or test_dataloader is not None, 'Please set track_test_stats to False or specify test dataloader.'
         # iterate over epochs
         for epoch in range(epochs):
@@ -202,17 +198,11 @@ class LSTM(nn.Module):
             self.train_epoch(train_dataloader, loss_criterion, optimizer)
             # optionally track training stats
             if track_train_stats:
-                if epoch % test_every == 0:
-                    train_loss, train_accuracy = self.evaluate(train_dataloader, loss_criterion)
-                    self.train_losses.append(train_loss)
-                    self.train_accuracies.append(train_accuracy)
-                    if verbose:
-                        print('Train Loss: ' + str(round(train_loss, 3)) + '. Train Accuracy: ' + str(round(train_accuracy, 3)))
+                train_loss, train_accuracy = self.evaluate(train_dataloader, loss_criterion)
+                if verbose:
+                    print('Train Loss: {0:.3f}. Train Accuracy: {1:.3f}'.format(train_loss, train_accuracy))
             # optionally track test stats
             if track_test_stats:
-                if epoch % test_every == 0:
-                    test_loss, test_accuracy = self.evaluate(test_dataloader, loss_criterion)
-                    self.test_losses.append(test_loss)
-                    self.test_accuracies.append(test_accuracy)
-                    if verbose:
-                        print('Test Loss:  ' + str(round(test_loss, 3)) + '. Test Accuracy:  ' + str(round(test_accuracy, 3)))
+                test_loss, test_accuracy = self.evaluate(test_dataloader, loss_criterion)
+                if verbose:
+                    print('Test Loss:  {0:.3f}. Test Accuracy:  {1:.3f}'.format(test_loss, test_accuracy))
